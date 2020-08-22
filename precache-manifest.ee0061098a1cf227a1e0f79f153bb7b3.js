9ce38633a85ffa5cb8dcc4ca8eeb8e25321e77f2
Tanushree Chaubal <tanushree2492@gmail.com>
2020-08-22 21:54:44 +0530

    Updates

---
+++ precache-manifest.ee0061098a1cf227a1e0f79f153bb7b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66aea75c677a22f68ea28cf4cb91f82f",
+    "url": "/tcportfolio/index.html"
+  },
+  {
+    "revision": "f876f113dc5044ddb4f9",
+    "url": "/tcportfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aac9bd0ab5fdda10f86d",
+    "url": "/tcportfolio/static/js/2.18af01bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tcportfolio/static/js/2.18af01bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f876f113dc5044ddb4f9",
+    "url": "/tcportfolio/static/js/main.86b607a7.chunk.js"
+  },
+  {
+    "revision": "60d9429ca5c23bd79989",
+    "url": "/tcportfolio/static/js/runtime-main.74d06413.js"
+  }
+]);
